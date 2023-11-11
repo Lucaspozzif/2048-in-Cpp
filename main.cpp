3a893e2fd45ac054ab0c81e5b81bb9c99f766c83
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-11 18:37:09 -0300

    main organized

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
-#include "minigames/2048/g2048.h"
-
-#include <iostream>
-#include <string>
-
-bool is_number(const std::string &s)
-{
-    return !s.empty() && std::find_if(s.begin(),
-                                      s.end(), [](unsigned char c)
-                                      { return !std::isdigit(c); }) == s.end();
-}
+#include "apps/2048/g2048.cpp"
+#include "apps/notes/notes.cpp"
 
 int main()
 {
-    bool validSize;
-    std::string stringInput;
-    while (!validSize)
-    {
-        std::cout << "Qual vai ser o tamanho do seu jogo? ";
-        std::getline(std::cin, stringInput);
-        if (is_number(stringInput))
-        {
-            validSize = true;
-        }
-    }
-    g2048 game(stoi(stringInput));
+    g2048 game;
     game.playGame();
 
     return 0;
